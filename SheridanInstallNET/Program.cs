37ff985ef55fd7645baf7a923267d0879bcbeb1c
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-01-08 10:50:03 -0500

    Imported ToboPasswords

--- SheridanInstallNET/Program.cs
+++ SheridanInstallNET/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
-using static SheridanInstallNET.Keyboard;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace SheridanInstallNET
 {
