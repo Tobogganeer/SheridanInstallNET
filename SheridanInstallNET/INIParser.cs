482a6277a6c383bde5664a84b22e149bc648e37a
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-01-14 22:10:39 -0500

    Loaded login files

--- SheridanInstallNET/INIParser.cs
+++ SheridanInstallNET/INIParser.cs
@@ -10,7 +10,7 @@ namespace SheridanInstallNET
     {
         static readonly char[] SplitChar = { '=' };
 
-        public static bool TryGetValue(List<string> lines, string key, out string value)
+        public static bool TryGetValue(List<string> lines, string key, out string value, string defaultValue = null)
         {
             for (int i = 0; i < lines.Count; i++)
             {
@@ -19,25 +19,31 @@ namespace SheridanInstallNET
                     string[] sides = lines[i].Split(SplitChar, 2, StringSplitOptions.None);
                     if (sides.Length == 2)
                     {
-                        value = sides[1];
+                        value = sides[1].Trim();
                         return true;
                     }    
                 }
             }
 
-            value = null;
+            value = defaultValue;
             return false;
         }
 
-        public static bool TryGetBool(List<string> lines, string key, out bool value)
+        public static bool TryGetBool(List<string> lines, string key, out bool value, bool defaultValue = false)
+            => TryGet(lines, key, out value, defaultValue, bool.TryParse);
+
+        public static bool TryGetInt(List<string> lines, string key, out int value, int defaultValue = 0)
+            => TryGet(lines, key, out value, defaultValue, int.TryParse);
+
+        public static bool TryGet<T>(List<string> lines, string key, out T value, T defaultValue, InOut.TryParse<T> tryParse)
         {
             if (!TryGetValue(lines, key, out string textValue))
             {
-                value = false;
+                value = defaultValue;
                 return false;
             }
 
-            return bool.TryParse(textValue, out value);
+            return tryParse(textValue, out value);
         }
     }
 }
--- SheridanInstallNET/LoginFile.cs
+++ SheridanInstallNET/LoginFile.cs
@@ -9,24 +9,101 @@ namespace SheridanInstallNET
 {
     public class LoginFile
     {
-        public List<string> Lines { get; private set; }
+        public string Name;
+        public string Category;
+        public List<string> Lines;
+
+        public int Order;
         public int CurrentLine { get; private set; }
 
-        private static readonly string Extension = ".login";
+        private static readonly string Extension = "login";
+        private static readonly string CategoryKey = "Category";
+        private static readonly string OrderKey = "Order";
 
-        public static LoginFile Load(string relativePath)
+        private static readonly string BlankFile = @"
+### Config
+# Category is used to group files for easy enabling/disabling
+Category=
+# Lower orders are loaded first
+Order=
+
+
+### Commands
+# type [text] - types the given text
+# typeenter [text] - types and then hits enter
+# enter - hits enter
+# tab - hits tab (useful for navigating between controls)
+# tab [amount] - hits tab a bunch of times
+# tabenter - hits tab and then enter
+
+# open [program] - opens the given program
+# wait [seconds] - delays operation for the given amount of time
+# goto [url] - opens Google and goes to that URL
+
+# win [key] - presses window key + key
+# ctrl [key] - presses ctrl + key
+# shift [key] - presses shift + key
+# up - presses up arrow key
+# down - presses down arrow key
+
+# Commands go here...
+goto slate.sheridancollege.ca
+ctrl t
+typeenter hi :3
+";
+
+
+        public LoginFile(string[] lines, string path)
         {
+            Name = Path.GetFileNameWithoutExtension(path);
+            Lines = new List<string>(lines);
 
+            INIParser.TryGetValue(Lines, CategoryKey, out Category);
+            INIParser.TryGetInt(Lines, OrderKey, out Order, -1);
         }
 
-        public static LoginFile[] LoadAll(string directory)
+
+        public static LoginFile Load(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] file = File.ReadAllLines(path);
+                return new LoginFile(file, path);
+            }
+            return null;
+        }
+
+        public static List<LoginFile> LoadAll(string directory)
         {
             directory = Path.Combine(Program.RootDirectory, directory);
 
             if (!Directory.Exists(directory))
             {
-
+                Directory.CreateDirectory(directory);
+                InOut.WriteLine($"{directory} not found.");
+                return null;
             }
+
+            List<LoginFile> files = new List<LoginFile>();
+
+            foreach (string filePath in Directory.EnumerateFiles(directory, $"*.{Extension}"))
+            {
+                LoginFile file = Load(filePath);
+                if (file != null)
+                    files.Add(file);
+            }
+
+            return files;
+        }
+
+        public static void CreateEmpty(string directory, string name)
+        {
+            directory = Path.Combine(Program.RootDirectory, directory);
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            File.WriteAllText(Path.Combine(directory, name + "." + Extension), BlankFile);
         }
     }
 }
