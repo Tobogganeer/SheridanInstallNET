601a3fead7d6e617582b7e2d89f16aa377db35eb
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-01-14 21:40:34 -0500

    Class to parse INI values

---
+++ SheridanInstallNET/INIParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheridanInstallNET
+{
+    public class INIParser
+    {
+        static readonly char[] SplitChar = { '=' };
+
+        public static bool TryGetValue(List<string> lines, string key, out string value)
+        {
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i].StartsWith(key))
+                {
+                    string[] sides = lines[i].Split(SplitChar, 2, StringSplitOptions.None);
+                    if (sides.Length == 2)
+                    {
+                        value = sides[1];
+                        return true;
+                    }    
+                }
+            }
+
+            value = null;
+            return false;
+        }
+
+        public static bool TryGetBool(List<string> lines, string key, out bool value)
+        {
+            if (!TryGetValue(lines, key, out string textValue))
+            {
+                value = false;
+                return false;
+            }
+
+            return bool.TryParse(textValue, out value);
+        }
+    }
+}
